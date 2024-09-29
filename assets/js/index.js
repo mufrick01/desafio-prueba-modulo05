6db3031791f1c8653312da8b56a3367805572904
mufrick01 <mrmufrick@gmail.com>
2024-09-29 17:06:06 -0300

    modal listo, falta actualizar preview del select

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ import { AnimalFactory } from './animal/AnimalFactory.js';
 
 let AnimalesRepositorio = [];
 const animalFactory = new AnimalFactory();
+const modalElement = document.getElementById('exampleModal')
+const modal = new bootstrap.Modal(modalElement);
 
 const btnRegistrar = document.querySelector("#btnRegistrar");
 
@@ -22,23 +24,39 @@ const ActualizarTablaEnVista = () => {
       imgAnimal.width = 200;
       imgAnimal.height = 300;
 
-      
+      imgAnimal.addEventListener('click', () => {
+         const modalBody = modalElement.querySelector('.modal-body')
+
+         modalBody.innerHTML= `     
+         <div class="d-flex flex-column text-white p-5">
+            <h2>${animal.getNombre()}</h2>
+            <img src="/assets/imgs/${animal.getImg()}" class="mx-auto my-3" style="height: 500px; width: 400px;">
+            <h4>Edad del animal:</h4>
+            <p>${animal.getEdad()}</p>
+            <h4>Descripción:</h4>
+            <p>${animal.getComentarios()}</p>
+           </div>  
+         `
+         modal.show();
+      })
+
+
       const button = document.createElement('button');
       button.classList.add('w-100', 'bg-dark', 'd-flex');
 
 
-      button.addEventListener('click',()=>{
+      button.addEventListener('click', () => {
          const playerAudio = document.querySelector('#player')
          playerAudio.src = `/assets/sounds/${animal.getSonido()}`
          playerAudio.play();
       })
-      
+
       const audioImg = document.createElement('img');
       audioImg.src = '/assets/imgs/audio.svg';
       audioImg.alt = '';
       audioImg.classList.add('mx-auto');
       audioImg.style.height = '50px';  // Asignar la altura
-      
+
       containerDiv.appendChild(imgAnimal);
       button.appendChild(audioImg);
       containerDiv.appendChild(button);
@@ -64,5 +82,7 @@ const registrarAnimal = async () => {
 
    ActualizarTablaEnVista();
 }
+
+const ActualizarAnimalPreview=()=>{}
 
 btnRegistrar.addEventListener("click", registrarAnimal)
