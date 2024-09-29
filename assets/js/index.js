bd047b1122d1a1b7b70c34423a68446e8615af67
mufrick01 <mrmufrick@gmail.com>
2024-09-29 16:46:51 -0300

    tabla lista, sonido listo, falta  abrir modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,19 +6,61 @@ const animalFactory = new AnimalFactory();
 const btnRegistrar = document.querySelector("#btnRegistrar");
 
 
-const ActualizarTablaEnVista = () =>{}
+const ActualizarTablaEnVista = () => {
+   const tablaAnimales = document.querySelector('#Animales')
+   tablaAnimales.innerHTML = '';
+   if (AnimalesRepositorio.length === 0) { return }
 
-const registrarAnimal = async () =>{
+   AnimalesRepositorio.map((animal) => {
+
+      const containerDiv = document.createElement('div');
+      containerDiv.classList.add('d-flex', 'flex-column');
+
+      const imgAnimal = document.createElement('img');
+      imgAnimal.src = `/assets/imgs/${animal.getImg()}`;
+      imgAnimal.alt = '';
+      imgAnimal.width = 200;
+      imgAnimal.height = 300;
+
+      
+      const button = document.createElement('button');
+      button.classList.add('w-100', 'bg-dark', 'd-flex');
+
+
+      button.addEventListener('click',()=>{
+         const playerAudio = document.querySelector('#player')
+         playerAudio.src = `/assets/sounds/${animal.getSonido()}`
+         playerAudio.play();
+      })
+      
+      const audioImg = document.createElement('img');
+      audioImg.src = '/assets/imgs/audio.svg';
+      audioImg.alt = '';
+      audioImg.classList.add('mx-auto');
+      audioImg.style.height = '50px';  // Asignar la altura
+      
+      containerDiv.appendChild(imgAnimal);
+      button.appendChild(audioImg);
+      containerDiv.appendChild(button);
+
+
+      tablaAnimales.appendChild(containerDiv);
+
+   })
+
+}
+
+const registrarAnimal = async () => {
    const animalName = document.querySelector("#animal").value;
    const edad = document.querySelector("#edad").value;
    const comentarios = document.querySelector("#comentarios").value;
-   
+
    const newAnimal = await animalFactory.create(animalName, edad, comentarios);
-   
-   if(!newAnimal){return}
+
+   if (!newAnimal) { return }
    AnimalesRepositorio.push(newAnimal)
 
-   AnimalesRepositorio.map((animal)=>console.log(animal))
+   AnimalesRepositorio.map((animal) => console.log(animal))
 
    ActualizarTablaEnVista();
 }
